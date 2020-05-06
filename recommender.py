013488687d9276dbbf241fbf91a1aee043a15815
Jake Allen <32147564+jakeja2@users.noreply.github.com>
2020-05-06 11:18:38 -0700

    Add files via upload

--- recommender.py
+++ recommender.py
@@ -15,59 +15,85 @@ import sys
 from time import time
 
 import numpy as np
+import json
 import pandas as pd
 
 # mess around with these variables to get
 # different answers
-clusters = 6
-hashing = True
-minibatch = False
+clusters = 4
+hashing = False
+minibatch = True
 features = 5000
-rows = 2000
+rows = 1002
 
-data = pd.read_json('data_temp.json')
+jake_vec = None
+tfidf = None
+
+data = pd.read_json('data_new.json')
 x = data.iloc[:, [0]].values
 
 # print(x[1][0])
 
-df = pd.DataFrame(columns=['journal', 'author', 'title', 'abstract'])
+df = pd.DataFrame(columns=['title', 'authors'])
+
+arr = []
+common = ["the", "of", "to", "and", "a", "that", "is", "it", "are", "this",
+          "there", "were", "be", "at", "had", "or", "as", "been"]
 
 for i in np.arange(0, rows):
-    new_title = x[i][0]['title'].replace('"', "")
-    tup = {'journal': x[i][0]['journal'],
-           'author': x[i][0]['authors'][0],
-           #'date': x[i][0]['date'],
-           'title': new_title,
-           'abstract': x[i][0]['abstract']
-           }
-    df = df.append(tup, ignore_index=True)
 
-arr = df.iloc[:, [0, 1, 2]].values
+    # need to create all of the authors
+    author_string = ""
+    for j in range(len(x[i][0]['authors'])):
+        author_string += (x[i][0]['authors'][j] + " ")
+
+    # need to create a new string based on frequency
+    abs_string = ""
+    for k in x[i][0]['word_count']:
+        if k not in common:
+            for l in range(int(x[i][0]['word_count'][k])):
+                abs_string += (k + " ")
+
+    new_title = x[i][0]['title']
+
+    final = ""
+    for val in range(5):
+        final += x[i][0]['journal'] + " " + author_string + " " + new_title + " "
+
+    final += abs_string
+
+    #print(final)
+
+    arr.append(final)
+    new_row = {'title': new_title, 'authors': x[i][0]['authors']}
+    df = df.append(new_row, ignore_index=True)
+
+
+#print(arr)
 
 # we now have our dataset split into rows
 
 # good option
 if hashing:
     # Perform an IDF normalization on the output of HashingVectorizer
-    hasher = HashingVectorizer(n_features=features,
-                               stop_words='english', alternate_sign=False,
-                               norm=None)
-    vectorizer = make_pipeline(hasher, TfidfTransformer())
-else:
-    hasher = HashingVectorizer(n_features=features,
-                               stop_words='english', alternate_sign=False,
-                               norm=None)
     vectorizer = HashingVectorizer(n_features=features,
                                    stop_words='english',
                                    alternate_sign=False, norm='l2')
+else:
+    vectorizer = TfidfVectorizer(max_df=0.5, max_features=features,
+                                 min_df=2, stop_words='english',
+                                 use_idf=True)
 
 
+X = vectorizer.fit_transform(arr)
 
-# vectorizer = HashingVectorizer(n_features=10000,
-#                                        stop_words='english',
-#                                        alternate_sign=False, norm='l2')
-vectorizer1 = TfidfVectorizer(hasher)
-X = vectorizer1.fit_transform(arr.ravel())
+# lets do componenet analysis
+# svd = TruncatedSVD(4)
+# normalizer = Normalizer(copy=False)
+# lsa = make_pipeline(svd, normalizer)
+#
+# X = lsa.fit_transform(X)
+
 
 if minibatch:
     km = MiniBatchKMeans(n_clusters=clusters, init='k-means++', n_init=1,
@@ -82,20 +108,74 @@ km.fit(X)
 
 # here should be where I test my code
 
-print("Top terms per cluster:")
-
-order_centroids = km.cluster_centers_.argsort()[:, ::-1]
-
-terms = vectorizer1.get_feature_names()
-for i in range(clusters):
-    print("Cluster %d:" % (i+1), end='')
-    for ind in order_centroids[i, :10]:
-        print(' %s' % terms[ind], end='')
-    print()
+# print("Top terms per cluster:")
+#
+# order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+#
+# terms = vectorizer.get_feature_names()
+# for i in range(clusters):
+#     print("Cluster %d:" % (i+0), end='')
+#     for ind in order_centroids[i, :10]:
+#         print(' %s' % terms[ind], end='')
+#     print()
 
 # TODO : run k-nearest neighbors to return some journals
-# pred = ['AIDS', "journal", "science"]
-# Y = vectorizer1.fit_transform(pred)
-# print(km.predict(Y))
+# my definiton -- medicine, science, math, engineering
 
+choices = pd.read_json('survey.json')
+author_name = pd.read_json('author_name.json')
+df_labels = pd.DataFrame(km.labels_)
 
+prediction_possibilities = ["patients dose cancer AIDS hiv dna rats youth development youth methods",
+                            "chemistry dna hiv complexes surgery neurosurgery health flame heat hybrid compounds ",
+                            "stat statistics energy solar field power market performance phase ",
+                            "performance electrical engineering computer science renewable heat forces "]
+
+# print(km.predict(vectorizer.transform(prediction_possibilities)))
+
+# print(choices.head())
+# print("")
+# print(author_name.head())
+
+# now i have usable json files
+query_string = ""
+
+if choices.iloc[0]["medicine"] == 1:
+    query_string += prediction_possibilities[0]
+if choices.iloc[0]["science"] == 1:
+    query_string += prediction_possibilities[1]
+if choices.iloc[0]["math"] == 1:
+    query_string += prediction_possibilities[2]
+if choices.iloc[0]["engineering"] == 1:
+    query_string += prediction_possibilities[3]
+
+cluster_pref = km.predict(vectorizer.transform([query_string]))
+author_pref = author_name.author_name.unique()
+
+# this will be my only return statement
+results = []
+no_dup = []
+
+df['cluster'] = df_labels
+
+for index, row in df.iterrows():
+    if row['cluster'] == cluster_pref and any(x in author_pref for x in row['authors']):
+        # add it to results
+        value = (index, row['title'], row['authors'][0])
+        results.append(value)
+        no_dup.append(value)
+        if len(results) >= 10:
+            break
+
+# hopefully we do not have to get to this point
+for index, row in df.iterrows():
+    if row['cluster'] == cluster_pref:
+        # add it to results
+        value = (index, row['title'], row['authors'][0])
+        if value not in no_dup:
+            results.append(value)
+        if len(results) >= 10:
+            break
+
+print(results)
+# paper_id, title
